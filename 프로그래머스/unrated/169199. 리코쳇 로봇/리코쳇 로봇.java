dd30deffeacb20d86abb5e17c21488f58fd126f2
HoonSub Kim <kimhunsope@naver.com>
2023-03-24 23:45:26 +0900

    [unrated] Title: 리코쳇 로봇, Time: 0.57 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -7,37 +7,41 @@ class Solution {
     public int solution(String[] board) {
         n = board.length;
         m = board[0].length();
-        boolean[][] visit = new boolean[n][m];
+        char[][] map = new char[n][m];
+        
         int[] start = {0, 0};
         // 시작 지점 찾기
         for (int i = 0; i < n; i++) {
+            map[i] = board[i].toCharArray();
             for (int j = 0; j < m; j++) {
-                if (board[i].charAt(j) == 'R') {
+                if (map[i][j] == 'R') {
                     start[0] = i;
                     start[1] = j;
                 }
             }
         }
         
-        return bfs(start[0], start[1], visit, board);
+        return bfs(start[0], start[1], map, board);
     }
     
-    public int bfs(int x, int y, boolean[][] visit, String[] board) {
+    public int bfs(int x, int y, char[][] map, String[] board) {
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{x, y, 0});
         
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             
-            if (visit[cur[0]][cur[1]]) {
+            // 방문했던 적이 있으면 패스
+            if (map[cur[0]][cur[1]] == 'X') {
                 continue;
             }
-            visit[cur[0]][cur[1]] = true;
+            map[cur[0]][cur[1]] = 'X';
             
             for (int[] move : MOVE) {
                 int nX = cur[0];
                 int nY = cur[1];
                 
+                // 끝을 만나거나, D를 만날 때까지 직진해야 한다.
                 while (true) {
                     nX += move[0];
                     nY += move[1];
@@ -53,6 +57,7 @@ class Solution {
                     continue;
                 }
                 
+                // 만약 목표지점에 도달했다면 끝
                 if (board[nX].charAt(nY) == 'G') {
                     return cur[2] + 1;
                 }
