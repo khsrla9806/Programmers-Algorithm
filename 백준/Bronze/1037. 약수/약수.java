e277962402e09e8cef68b3bf126b3d63f8fa06e4
HoonSub Kim <kimhunsope@naver.com>
2023-03-26 02:38:30 +0900

    [Bronze I] Title: 약수, Time: 120 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -1,18 +1,18 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int count = Integer.parseInt(br.readLine());
-        int index = 0;
+        br.readLine();
+        int min = Integer.MAX_VALUE;
+        int max = 0;
 
-        int[] data = new int[count];
         for (String value : br.readLine().split(" ")) {
-            data[index++] = Integer.parseInt(value);
+            int v = Integer.parseInt(value);
+            min = Math.min(min, v);
+            max = Math.max(max, v);
         }
-        Arrays.sort(data);
-        System.out.println(data[0] * data[count - 1]);
+        System.out.println(min * max);
     }
 }
