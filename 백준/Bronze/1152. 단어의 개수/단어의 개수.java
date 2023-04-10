69c29fc6f75814537f3ae7028f8c972211b26694
HoonSub Kim <kimhunsope@naver.com>
2023-04-10 14:10:59 +0900

    [Bronze II] Title: 단어의 개수, Time: 268 ms, Memory: 20036 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine(), " ");
+        System.out.println(tokenizer.countTokens());
+    }
+}
