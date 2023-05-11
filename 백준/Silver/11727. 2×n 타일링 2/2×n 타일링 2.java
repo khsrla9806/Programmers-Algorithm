8b3f0e2b5697203e7e149b2965bbad626f33055e
HoonSub Kim <kimhunsope@naver.com>
2023-05-11 23:55:07 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            if (i % 2 == 0) {
+                dp[i] = (dp[i - 1] * 2 + 1) % 10007;
+            } else {
+                dp[i] = (dp[i - 1] * 2 - 1) % 10007;
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
