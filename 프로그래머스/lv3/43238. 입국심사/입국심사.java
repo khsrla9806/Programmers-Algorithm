c5b9bc9f94ecab491b1c329ac075c542421896a6
HoonSub Kim <kimhunsope@naver.com>
2023-05-19 14:35:12 +0900

    [level 3] Title: 입국심사, Time: 0.38 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long left = 0;
+        long right = (long) n * times[times.length - 1];
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long sum = 0;
+            for (int i = 0; i < times.length; i++) {
+                sum += mid / times[i];
+            }
+            if (sum >= n) {
+                right = mid - 1;
+                answer = mid;
+            } else if (sum < n) {
+                left = mid + 1;
+            }
+        }
+        return answer;
+    }
+}
