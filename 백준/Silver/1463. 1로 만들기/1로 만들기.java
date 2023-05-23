8b7f291f2d6035c96bee23edca0f8a6a43263cae
HoonSub Kim <kimhunsope@naver.com>
2023-05-23 16:41:19 +0900

    [Silver III] Title: 1로 만들기, Time: 156 ms, Memory: 18252 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,20 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
+    static int N;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(reader.readLine());
-        int[] map = new int[n + 1];
-        Arrays.fill(map, Integer.MAX_VALUE);
-        map[map.length - 1] = 0;
-
-        for (int i = map.length - 1; i > 0; i--) {
-            int cnt = map[i] + 1;
-
-            if (i % 3 == 0) map[i / 3] = Math.min(map[i / 3], cnt);
-            if (i % 2 == 0) map[i / 2] = Math.min(map[i / 2], cnt);
-            map[i - 1] = Math.min(map[i - 1], cnt);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[N] = 0;
+        for (int i = N; i > 0; i--) {
+            dp[i / 3] = i % 3 == 0 ? Math.min(dp[i / 3], dp[i] + 1) : dp[i / 3];
+            dp[i / 2] = i % 2 == 0 ? Math.min(dp[i / 2], dp[i] + 1) : dp[i / 2];
+            dp[i - 1] = Math.min(dp[i - 1], dp[i] + 1);
         }
-
-        System.out.println(map[1]);
+        System.out.println(dp[1]);
     }
 }
