d9581d3d72b4dbdb36e130c8dc3cf742db062b54
Hoonsub Kim <kimhunsope@naver.com>
2023-12-16 00:09:58 +0900

    [Gold II] Title: 파일 탐색기, Time: 1792 ms, Memory: 345436 KB -BaekjoonHub

---
+++ 백준/Gold/20210. 파일 탐색기/파일 탐색기.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+        PriorityQueue<Value> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            queue.add(new Value(reader.readLine()));
+        }
+
+        StringBuilder builder = new StringBuilder();
+        while (!queue.isEmpty()) {
+            builder.append(queue.poll()).append("\n");
+        }
+        System.out.println(builder);
+    }
+}
+
+class Value implements Comparable<Value> {
+    String value;
+    ArrayList<String> seperatedValue;
+
+    public Value(String value) {
+        this.value = value;
+        setSeperatedValue(value);
+    }
+
+    private void setSeperatedValue(String value) {
+        this.seperatedValue = new ArrayList<>();
+        StringBuilder tempBuilder = new StringBuilder();
+
+        for (int i = 0; i < value.length(); i++) {
+            char temp = value.charAt(i);
+
+            if (Character.isDigit(temp)) {
+                tempBuilder.append(temp);
+                continue;
+            } else if (tempBuilder.length() > 0) { // 숫자 아니고, 빌더에 값이 있는 경우
+                seperatedValue.add(tempBuilder.toString());
+                tempBuilder = new StringBuilder();
+            }
+            seperatedValue.add(String.valueOf(temp));
+        }
+
+        if (tempBuilder.length() > 0) { // 다 돌고 빌더에 남아 있는 것이 있으면 마저 넣어줌
+            seperatedValue.add(tempBuilder.toString());
+        }
+    }
+
+    @Override
+    public int compareTo(Value other) {
+        ArrayList<String> seperatedValueOfOther = other.seperatedValue;
+        int smallSize = Math.min(seperatedValueOfOther.size(), this.seperatedValue.size());
+        for (int i = 0; i < smallSize; i++) {
+            String valueOfThis = this.seperatedValue.get(i);
+            String valueOfOther = seperatedValueOfOther.get(i);
+
+            BigDecimal decimalOfThis = convertToDecimal(valueOfThis);
+            BigDecimal decimalOfOther = convertToDecimal(valueOfOther);
+
+            // 둘 다 숫자인 경우
+            if (decimalOfThis != null && decimalOfOther != null) {
+                if (decimalOfThis.compareTo(decimalOfOther) == 0) {
+                    // 값도 같고, 0의 개수도 같은 경우
+                    if (valueOfThis.length() == valueOfOther.length()) {
+                        continue;
+                    }
+                    return valueOfThis.length() - valueOfOther.length();
+                }
+                return decimalOfThis.compareTo(decimalOfOther);
+            }
+
+            // 둘 중 하나만 숫자인 경우
+            if (decimalOfThis != null || decimalOfOther != null) {
+                return decimalOfThis != null ? -1 : 1;
+            }
+
+            // 둘 다 문자인 경우
+            int priorityOfThis = AlphabetComputer.getPriority(valueOfThis);
+            int priorityOfOther = AlphabetComputer.getPriority(valueOfOther);
+
+            if (priorityOfThis == priorityOfOther) {
+                continue;
+            }
+
+            return priorityOfThis - priorityOfOther;
+        }
+
+        return this.value.length() - other.value.length();
+    }
+
+    private BigDecimal convertToDecimal(String value) {
+        try {
+            return new BigDecimal(value);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
+
+class AlphabetComputer {
+    static Map<String, Integer> alphabetPriority = new HashMap<>();
+
+    static {
+        int priority = 0;
+        int aCode = 'a';
+        int zCode = 'z';
+        for (int i = aCode; i <= zCode; i++) {
+            char ch = (char) i;
+            String alphabet = String.valueOf(ch);
+            alphabetPriority.put(alphabet.toUpperCase(), priority++);
+            alphabetPriority.put(alphabet, priority++);
+        }
+    }
+
+    public static int getPriority(String alphabet) {
+        return alphabetPriority.get(alphabet);
+    }
+}
