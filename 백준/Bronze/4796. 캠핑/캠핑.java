ad4d8a7d818cc3e200336ce132326e2461643e18
Hoonsub Kim <kimhunsope@naver.com>
2024-01-12 13:45:53 +0900

    [Bronze I] Title: 캠핑, Time: 348 ms, Memory: 22144 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int testCase = 1; ; testCase++) {
+            String[] command = reader.readLine().split(" ");
+            if (command[0].equals("0") && command[1].equals("0") && command[2].equals("0")) {
+                break;
+            }
+            int L = Integer.parseInt(command[0]);
+            int P = Integer.parseInt(command[1]);
+            int V = Integer.parseInt(command[2]);
+            int answer = (V / P) * L + Math.min(V % P, L);
+
+            System.out.printf("Case %d: %d\n", testCase, answer);
+        }
+    }
+}
